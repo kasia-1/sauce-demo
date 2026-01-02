649b78730bbe5c389f519e8b0282f2a83a911041
katarzyna.klosowska <katarzyna.klosowska@sanoma.com>
2026-01-02 14:36:25 +0100

    add openAProductDetailsPage.spec.cy.ts, add inventoryPage.ts, add inventoryPage.ts with selectors

---
+++ cypress/e2e/product/openAProductDetailsPage.spec.cy.ts
@@ -0,0 +1,21 @@
+import { LoginPage } from "../../support/pageObjects/login/loginPage";
+import {InventoryPage} from "../../support/pageObjects/purchases/inventoryPage";
+import {InventoryItem} from "../../support/selectors/purchases/inventoryPage";
+import {InventoryItemPage} from "../../support/pageObjects/purchases/inventoryItemPage";
+
+describe('Product details', () => {
+    const loginPage: LoginPage = new LoginPage();
+    const inventoryPage: InventoryPage = new InventoryPage();
+    const inventoryItemPage: InventoryItemPage = new InventoryItemPage();
+
+    beforeEach(() => {
+        loginPage.login();
+        inventoryPage.isLoaded();
+    });
+
+    it('The user can open the product details by clicking on it on the list', () => {
+        inventoryItemPage.clickOnTheItemTitle(InventoryItem.backpack);
+        inventoryItemPage.isLoaded();
+        inventoryItemPage.urlContainsItemId();
+    });
+});
